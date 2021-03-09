192ab6ac6db1b9a9d12a003d5c6e0c7ea6c8e8e2
Austin <>
2021-03-09 03:38:13 -0500

    Update test to accomodate edge-case

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -12,4 +12,12 @@ class TranslatorTest < Minitest::Test
     assert_equal [".0", ".0", "00"], translator.english_to_braille("#")
     assert_equal [".0", ".0", "00"], translator.english_to_braille("0")
   end
+
+  def test_it_can_convert_from_braille_to_english
+    translator = Translator.new
+    assert_equal "a", translator.braille_to_english(["0.", "..", ".."])
+    assert_equal "b", translator.braille_to_english(["0.", "0.", ".."])
+    assert_equal " ", translator.braille_to_english(["..", "..", ".."])
+    assert_equal "g", translator.braille_to_english(["00", "00", ".."])
+  end
 end
